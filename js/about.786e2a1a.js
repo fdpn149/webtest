83fa62dbb8e9a223225b6bfbde2d09cd45424fb3
fdpn149 <nog3952149@gmail.com>
2024-02-22 16:51:01 +0800

    Updates

---
+++ js/about.786e2a1a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfront_end"]=self["webpackChunkfront_end"]||[]).push([[594],{5562:function(n,t,u){u.r(t),u.d(t,{default:function(){return f}});var e=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],r=u(7391),a={},l=(0,r.A)(a,e,s,!1,null,null,null),f=l.exports}}]);
+//# sourceMappingURL=about.786e2a1a.js.map
